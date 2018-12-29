9d890ead1353e89a4a0d348043ed5de49ec75c3b
samrtisong <44254768+samrtisong@users.noreply.github.com>
2018-12-29 20:49:54 +0800

    Create python_version.py

---
+++ 3-Selection_sort/python_version.py
@@ -0,0 +1,24 @@
+
+a = [2, 3, 1, 4, 5]
+temp = 0
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if a[i] > a[j]:
+            temp = a[j]
+            a[j] = a[i]
+            a[i] = temp
+print (a)                     # [1,2,3,4,5]
+
+
+for i in range(len(a)):
+    minindex = i
+    for j in range(i+1, len(a)):       # 正规选择排序
+        if a[i] > a[j]:                # 选出最小的数，并交换之
+            minindex = j
+        temp = a[minindex]
+        a[minindex] = a[i]
+        a[i] = temp
+print (a)                      # [1,2,3,4,5]
+
+
+
