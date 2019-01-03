c1fdbb356ac11036d415bd1bf9665463a3550606
samrtisong <44254768+samrtisong@users.noreply.github.com>
2019-01-03 21:33:51 +0800

    Update Python_version.py

--- 4-Insertion_sort/Python_version.py
+++ 4-Insertion_sort/Python_version.py
@@ -1,3 +1,17 @@
 #  插入排序算法
 #  时间复杂度： O(n²)
 #
+
+a = [2,3,1,4,5]
+
+for i in range(1, len(a)):
+	index = i
+	curr = a[i]
+	while curr < a[index-1]:
+		a[index] = a[index-1]
+		index= index-1
+		if index-1 < 0:
+			break
+	a[index] = curr
+
+print a
